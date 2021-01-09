d1295a370d409668e2ba95e05eb7bc6d013474df
allanbud <66875988+allanbud@users.noreply.github.com>
2021-01-09 10:50:15 +0200

    done with date format

--- store-front/src/app/components/my-profile/my-profile.component.ts
+++ store-front/src/app/components/my-profile/my-profile.component.ts
@@ -235,7 +235,6 @@ import {Order} from '../../models/order';
     this.orderService.getOrderList().subscribe(
       response => {
         this.orderList = JSON.parse(response);
-
       },
       error => {
         console.log(error.error);
